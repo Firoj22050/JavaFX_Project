1234a38cb24242f1df9504d4ffc2084d82f9d4d9
Firoj22050 <it22050@mbstu.ac.bd>
2025-06-13 20:14:48 +0600

    Update DBInfo.java

--- DBInfo.java
+++ DBInfo.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DBInfo {
     private static final String URL = "jdbc:mysql://localhost:3306/food_db?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-    private static final String USER = "Atif";
-    private static final String PASSWORD = "arpita";
+    private static final String USER = "Firoj";
+    private static final String PASSWORD = "2345";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
